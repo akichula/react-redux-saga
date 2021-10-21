9ba2e462b9415eb26bd12bdc636d9ace90b5adf7
akichula <masterfrag111@gmail.com>
2021-10-21 11:47:29 +0400

    Using fork for async requests

--- saga-project/src/redux/sagas/index.js
+++ saga-project/src/redux/sagas/index.js
@@ -1,23 +1,29 @@
-import {put, takeEvery, call} from 'redux-saga/effects';
+import {put, takeEvery, call, fork} from 'redux-saga/effects';
 
 async function getData(pattern) {
    const request = await fetch(`https://jsonplaceholder.typicode.com/${pattern}`);
     return await request.json()
 }
 
-export function* workerSaga() {
-
-    // For best result use call() func with async function
+export function* loadUsers() {
     const users = yield call(getData, 'users');
-    const todos = yield call(getData, 'todos');
     yield put({type: 'SET_USERS', payload: users})
+}
+
+export function* loadTodos() {
+    const todos = yield call(getData, 'todos');
     yield put({type: 'SET_TODOS', payload: todos})
 }
 
-export function* watchClickSaga() {
+export function* workerSaga() {
+    yield fork(loadUsers);
+    yield fork(loadTodos);
+}
+
+export function* watchLoadDataSaga() {
     yield takeEvery('LOAD_DATA', workerSaga);
 }
 
 export default function*  rootSaga() {
-    yield watchClickSaga();
+    yield fork(watchLoadDataSaga);
 }
